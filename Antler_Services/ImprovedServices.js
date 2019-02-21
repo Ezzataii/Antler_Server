557c7eda5392dce6c9f26561ca83c295bdbbddae
unknown <jadismail7@gmail.com>
2019-02-22 00:25:46 +0200

    Changes to be committed: 	modified:   Antler_Services/ImprovedServices.js

--- Antler_Services/ImprovedServices.js
+++ Antler_Services/ImprovedServices.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var app = express(); 
 var mysql = require('mysql');
 var formidable = require('formidable');
-var dir = "content/display1/";
+var dir = "./";
 var fs = require('fs');
 const parse = require("./parse");
 const adminServices = require('./AdminFunctions');
@@ -15,11 +15,11 @@ const userServices = require('./UserFunctions');
 
 
 // Body Parser Middleware
-app.use(bodyParser.json()); 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //CORS Middleware
 app.use(function (req, res, next) {
-    //Enabling CORS 
+    //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
@@ -41,9 +41,10 @@ var DBconf = {
 };
 var con = mysql.createConnection(DBconf);
 //Function to connect to database and execute query
-var  executeQuery = function(res, query){             
-    con.query(query, function(error, rows, result) {
+    function executeQuery(res, query){
+    con.query(query, function(err, rows, result) {
         console.log(query);
+        if (err) throw err;
         res.end(JSON.stringify(rows));
         res.end();
     });
@@ -51,33 +52,40 @@ var  executeQuery = function(res, query){
 
 //GET API
 app.get("/api/:table", function(req , res){
-    var query = adminServices.get(req.params.table, req.query);
+    var query = adminServices.get(req.params.table, req.query);     //100% done
     executeQuery (res, query);
 });
 
 //POST API
 app.post("/:api/:table", function(req , res){
     var query = "";
-    if (req.params.table == "IMAGE"){
+    if (req.params.api == "UPLOAD"){
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             var oldpath = files.filetoupload.path;              //uploading file to server
-            var newpath = dir + files.filetoupload.name;        
-            fs.rename(oldpath, newpath, parse.done);
+            var newpath = dir + files.filetoupload.name;
+            query = adminServices.insert(req.params.table,{'name':files.filetoupload.name,'user':'55'})         //Needs Dynamic User
+            fs.rename(oldpath, newpath, (err)=>{
+                if (err) throw err;
+                console.log('File uploaded and moved!');
+                executeQuery (res, query);
+            });
         });
+    }else{
+        console.log(req.body.parameters);
+        query = adminServices.insert(req.params.table,JSON.parse(req.body.parameters));                         //100% done
+        executeQuery (res, query);
     }
-    var query = adminServices.insert(req.params.table,req.body.parameters);
-    executeQuery (res, query);
 });
 
 //PUT API
  app.put("/api/:table/:id", function(req , res){
-    var query = adminServices.update(req.params.table,req.body,req.params.id);
+    var query = adminServices.update(req.params.table,req.body,req.params.id);                                  //Untouched
     executeQuery (res, query);
 });
 
 // DELETE API
  app.delete("/api/:table/:id", function(req , res){
-    var query =adminServices.remove(req.params.table,req.params.id);
+    var query =adminServices.remove(req.params.table,req.params.id);                                            //Untouched
     executeQuery (res, query);
 });
