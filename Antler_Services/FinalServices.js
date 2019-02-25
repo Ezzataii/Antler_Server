6ae1cb20bf573d80dcc741352003ec2bc3276eaf
unknown <jadismail7@gmail.com>
2019-02-25 19:32:50 +0200

    Changes to be committed: 	modified:   Antler_Services/FinalServices.js

--- Antler_Services/FinalServices.js
+++ Antler_Services/FinalServices.js
@@ -33,8 +33,8 @@ app.use(function (req, res, next) {
 //Initiallising connection string
 var DBconf = {
     host: "localhost",
-    user: "root",
-    password: "",
+    user: "commandeers",
+    password: "Commandeers1234",
     database: "Antler"
 };
 var con = mysql.createConnection(DBconf);
