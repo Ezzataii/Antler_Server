19cff492c9a51b3a559f5fea645d95ff3e3e425b
Ezzat Chebaro <ezzatchebaro@gmail.com>
2019-02-28 13:38:14 +0200

    Created new Vue project for webapp

---
+++ Antler_WebApp/antler-webapp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Nav/>
+
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+html body {
+  background-color: #333333;
+  color: #dddddd;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
