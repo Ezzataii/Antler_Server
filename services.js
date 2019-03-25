2236bc416f5e3259aa009f00d6d377cddabb52f3
unknown <jadismail7@gmail.com>
2019-03-25 21:13:16 +0200

    Even more bugfixes ^_^

--- services.js
+++ services.js
@@ -219,7 +219,7 @@ app.get("/view/ad/:adid",(req,res)=>{
 });
 
 app.get("/api/create/:group/:user",(req,res)=>{
-    executeQuery(res,`INSERT INTO GROUP(name,user) VALUES(${req.params.group},${req.params.user})`);
+    executeQuery(res,`INSERT INTO USERGROUP(name,user) VALUES(${req.params.group},${req.params.user})`);
 });
 
 app.post("/api/add/group/:groupid",(req,res)=>{
